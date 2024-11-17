5ee286165d0cd4ae43c2bed91d0465c661f01045
JiteshYadav10 <yadavjitesh890@gmail.com>
2024-11-17 21:37:44 +0530

    Add files via upload

---
+++ C Practice/assignment 1/q5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int num,i,sum=0,sum1;
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    for (i=1;i<=num;i=i+1) {
+        while(num%i==0) {
+        printf("%d, ",i);
+        sum=sum+i;
+        i=i+1;
+    }
+    }
+    sum1=sum-num;
+    
+
+    if (sum1==num)
+    printf("\n%d is a perfect number",num);
+    else 
+    printf("\n%d is not a perfect number",num);
+
+    return 0;
+}
